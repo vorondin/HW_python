4130938de4eb0d3e2a0b51b277b3c59f320533fe
YakovlevAndrey <andrey.odin@gmail.com>
2021-11-30 11:24:36 +0300

    Lesson1_1

--- pythonProject/Lesson_1_1/Lesson1_4.py
+++ pythonProject/Lesson_1_1/Lesson1_4.py
@@ -1,9 +1,10 @@
-znach = input("введите значение: ")
+znach = int(input("введите значение: "))
 a = 0
 max = 0
 
-while a < len(znach):
-    if max < int(znach[a]):
-       max = int(znach[a])
-    a += 1
+while znach > 0 and max < 9:
+    a = znach % 10
+    max = a if max < a else max
+    znach = znach // 10
+
 print(max)
